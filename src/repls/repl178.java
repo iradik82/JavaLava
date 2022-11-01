b145ef29ec727f301c2264a2cc6409137cd3b50a
iradik82 <radetskyy96@gmail.com>
2022-11-01 16:34:50 -0400

    Testing

---
+++ src/repls/repl178.java
@@ -0,0 +1,30 @@
+package repls;
+class EncapsulationDemo{
+    private String empName;
+    private int empAge;
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public int getEmpAge() {
+        return empAge;
+    }
+
+    public void setEmpAge(int empAge) {
+        this.empAge = empAge;
+    }
+}
+public class repl178 {
+    public static void main(String[] args) {
+        EncapsulationDemo bob=new EncapsulationDemo();
+        bob.setEmpName("John");
+        bob.setEmpAge(30);
+        System.out.println("Employee Name: "+bob.getEmpName());
+        System.out.println("Employee Age: "+bob.getEmpAge());
+    }
+}
